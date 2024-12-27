f83c785dc14e96631fda606135c5398323fdf62b
SeaCil04 <92726995+SeaCil04@users.noreply.github.com>
2024-12-27 16:45:27 +0700

    Update lab13_1.cpp
    kanitSUS

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,26 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int n,double b[]){
+    double mean = 0,SD = 0,meanG = 1,hmean = 0,max = 0,min =0;
+    for(int i = 0;i<n;i++){
+        mean += a[i];
+    }
+    b[0] = mean/n;
+    for(int i = 0;i<n;i++){
+        SD += pow(a[i]-b[0],2);
+    }
+    b[1] = sqrt(SD/n);
+    for(int i = 0;i<n;i++) meanG *= a[i];
+    b[2] = pow(meanG,1.0/n);
+    for(int i = 0;i<n;i++) hmean += 1.0/a[i];
+    b[3] = n/(hmean);
+    for(int i = 0;i<n;i++){
+        if(i == 0) {min = a[i];max = a[i];}
+        if(a[i] > max) max = a[i];
+        if(a[i] < min) min = a[i];
+    }
+    b[4] = max;
+    b[5] = min;
+}
