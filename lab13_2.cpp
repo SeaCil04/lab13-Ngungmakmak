e1fc3f351a969b48c791fb0766fe1a5af709b8cf
SeaCil04 <92726995+SeaCil04@users.noreply.github.com>
2024-12-27 16:47:20 +0700

    YaKMAKMAK

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,35 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool image[][M],int a,int b,int c){
+    if(a == 1){
+        image[b][c] = 1;
+    }else{
+        for(int i = 0;i<N;i++){
+            for(int j = 0;j<M;j++){
+                if(sqrt(pow(i-b,2)+pow(j-c,2)) <= a-1){
+                    image[i][j] = 1;
+                }
+            }
+        }
+    }
+}
+
+void showImage(const bool image[][M]){
+    cout << "------------------------------------------------------------------------" << endl;
+    for(int i = 0;i < N;i++){
+        cout << "|";
+        for(int j=0;j<M;j++){
+            if(image[i][j] == 1){
+                cout << "*";
+            }else {
+                cout <<" ";
+            }
+        }
+        cout << "|";
+        cout << endl;
+    }
+    cout << "------------------------------------------------------------------------"<<endl;
+}
+
